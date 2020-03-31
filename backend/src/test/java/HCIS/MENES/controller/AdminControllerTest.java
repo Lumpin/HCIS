e1622ec9cfb4231903db007204d942986c95f9f2
jonas <jonas.menesklou@online.de>
2020-03-31 14:53:21 +0200

    unit test for admin controller

---
+++ backend/src/test/java/HCIS/MENES/controller/AdminControllerTest.java
@@ -0,0 +1,24 @@
+package HCIS.MENES.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdminControllerTest {
+
+    @Test
+    void createPhysician() {
+    }
+
+    @Test
+    void deletePhysician() {
+    }
+
+    @Test
+    void getAllPatients() {
+    }
+
+    @Test
+    void deletePatient() {
+    }
+}
